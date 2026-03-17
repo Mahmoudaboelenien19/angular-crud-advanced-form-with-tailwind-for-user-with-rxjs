f7689be125e992abb32c05c501bef230825e86fa
Mahmoudaboelenien19 <m.aboelenien20020@gmail.com>
2026-03-17 17:56:52 +0200

    add view users list

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatChipsModule } from '@angular/material/chips';
 
+import { User } from '../../models/user.model';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-user-card',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatChipsModule,
+    CommonModule,
+  ],
   templateUrl: './user-card.component.html',
-  styleUrl: './user-card.component.css'
+  styleUrl: './user-card.component.css',
 })
 export class UserCardComponent {
-
+  @Input() user!: User;
+  onEdit() {}
+  onDelete() {}
 }
